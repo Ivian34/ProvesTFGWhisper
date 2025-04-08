7b1758f1557a3d6a19ebc5e55ac3324b147874d9
Ivan Cobos <ivancobosnavarro@gmail.com>
2025-04-08 23:43:09 +0200

    reorganizando y poniendo bien los path

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='prova_TFG',
+    version='0.1.0',
+    packages=find_namespace_packages(),  # Busca paquetes sin necesidad de __init__.py
+)
